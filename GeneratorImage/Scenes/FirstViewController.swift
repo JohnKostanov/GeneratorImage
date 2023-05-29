f52b674cb7847363f00685abc622d97191ebf63d
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-05-29 22:06:30 +0200

    Added textfield

--- GeneratorImage/Scenes/FirstViewController.swift
+++ GeneratorImage/Scenes/FirstViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter your request"
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         title = "First"
+        
+        view.addSubview(textField)
+        setupConstraints()
     }
-
-
+    
+    func setupConstraints() {
+        let constraints = [
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
