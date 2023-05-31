62bf3280957679b5bc163e1a10a46e5173353a3e
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-06-01 01:30:38 +0200

    Added func saveImages and loadSavedImages

--- GeneratorImage/Scenes/FavoriteImage/Controller/SecondViewController.swift
+++ GeneratorImage/Scenes/FavoriteImage/Controller/SecondViewController.swift
@@ -12,6 +12,9 @@ class SecondViewController: UIViewController {
     let cellIdentifier = "ImageCell"
     var favorites = FavoriteImage(maxAmount: 10)
     
+    // Создаем массив для хранения путей к файлам изображений
+    var imagePaths: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,6 +27,7 @@ class SecondViewController: UIViewController {
         view.addSubview(favoriteView.tableView)
         
         favoriteView.setupConstraints(view)
+        loadSavedImages()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,6 +36,56 @@ class SecondViewController: UIViewController {
     
     private func updateTableView() {
         favoriteView.tableView.reloadData()
+    }
+    
+    private func saveImages() {
+        // Получаем путь к директории для сохранения изображений
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+
+        // Сохраняем изображения на диск и добавляем пути к файлам в массив
+        for image in favorites.allImages {
+            guard let image else {
+                continue
+            }
+            // Создаем уникальное имя файла для изображения
+            let imageName = UUID().uuidString
+
+            // Создаем путь к файлу на диске
+            let imagePath = documentsDirectory.appendingPathComponent("\(imageName).png")
+
+            // Сохраняем изображение на диск
+            if let imageData = image.pngData() {
+                do {
+                    try imageData.write(to: imagePath)
+                    // Добавляем путь к файлу в массив
+                    imagePaths.insert(imagePath.path, at: 0)
+                } catch {
+                    print("Ошибка сохранения изображения на диск: \(error)")
+                }
+            }
+        }
+
+        // Сохраняем путь к файлу в UserDefaults
+        UserDefaults.standard.set(imagePaths, forKey: "SavedImagesPaths")
+    }
+    
+    private func loadSavedImages() {
+        // Получаем массив путей к файлам из UserDefaults
+        if let savedImagesPaths = UserDefaults.standard.array(forKey: "SavedImagesPaths") as? [String] {
+            // Загружаем каждое изображение с диска
+            var loadedImages: [UIImage] = []
+            for imagePath in savedImagesPaths {
+                if let savedImage = UIImage(contentsOfFile: imagePath) {
+                    loadedImages.append(savedImage)
+                }
+            }
+            if favorites.allImages.isEmpty {
+                favorites.allImages = loadedImages
+            }
+            print(savedImagesPaths)
+        }
     }
 }
 
@@ -60,6 +114,14 @@ extension SecondViewController: UITableViewDelegate {
         if editingStyle == .delete {
             favorites.allImages.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            do {
+                try FileManager.default.removeItem(atPath: imagePaths[indexPath.row])
+                imagePaths.remove(at: indexPath.row)
+                print("Изображение удалено успешно")
+            } catch {
+                print("Ошибка удаления изображения: \(error)")
+            }
         }
     }
     
@@ -74,6 +136,8 @@ extension SecondViewController: ImageDelegate {
         } else {
             favorites.allImages.insert(image, at: 0)
             favorites.allImages.removeLast()
+            imagePaths.removeLast()
         }
+        saveImages()
     }
 }
